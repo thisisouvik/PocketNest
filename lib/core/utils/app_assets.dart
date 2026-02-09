5a9a77f7e636f149956e072c0fd9bd046fb6c670
Souvik Mandal <souvikmandals10@gmail.com>
2026-02-09 19:24:56 +0530

    feat: Add AppAssets class for managing asset paths

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,13 @@
+
+class AppAssets {
+
+  static const String _images = 'assets/images';
+  static const String format = '.svg';
+
+  //Splash Screen
+  static const String splashWomen = '$_images/splash/splash_women$format';
+  static const String splashTextStyle = '$_images/splash/splashTextStyle$format';
+
+  //Logo
+  static const String appTextLogo = '$_images/branding/appTextLogo$format';
+}
